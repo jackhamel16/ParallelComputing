fc59a7deab84f511597de7350fed64b7fc2bd10b
Jack Hamel <jackhaml16@gmail.com>
2019-01-31 23:40:02 -0500

    updated

--- homework/2/timings.py
+++ homework/2/timings.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 filename = "./timings.dat"
 file = open(filename, 'r')
 
-sizes = np.array([200,1000,5000,8000])#,10000,15000,20000,40000])
+sizes = np.array([200,1000,5000,8000,10000,15000,20000,40000])
 line_list = []
 
 for line in file:
-    line_list.append(line)
+    line_list.append(np.double(line))
 
 times = np.array([np.zeros(len(sizes)) for i in range(3)])
     
@@ -17,7 +17,7 @@ for part_idx in range(3):
         for run_idx in range(3):
             line_idx = part_idx * len(sizes) * 3 + 3 * size_idx + run_idx
 #            print(part_idx, size_idx, run_idx, "   ", line_idx, line_list[line_idx])
-            times[part_idx][size_idx] += np.double(line_list[line_idx])
+            times[part_idx][size_idx] += line_list[line_idx]
 
 times = times / 3
             
@@ -25,9 +25,32 @@ plt.figure()
 
 for part_idx in range(3):
     plt.plot(sizes, times[part_idx])
-    
-plt.legend(["Part " + str(i) for i in range(3)])
-plt.title("Transpose Timing Study")
-plt.xlabel("Matrix Size")
-plt.ylabel("Run Time")
-plt.savefig('plot_timings.png')
+
+plt.legend(["Part " + str(i+1) for i in range(3)])
+plt.set_title("Transpose Timing Study")
+plt.set_xlabel("Matrix Size")
+plt.set_ylabel("Run Time")
+#fig, axes = plt.subplots(2,1)
+#
+#for part_idx in range(3):
+#    axes[0].plot(sizes, times[part_idx])
+#
+#axes[0].legend(["Part " + str(i+1) for i in range(3)])
+#axes[0].set_title("Transpose Timing Study")
+#axes[0].set_xlabel("Matrix Size")
+#axes[0].set_ylabel("Run Time")
+#
+#col_labels = ["Basic", "Blocked", "Cache Oblivious"]
+#row_labels = ["MatSize = {0:d}".format(sizes[i]) for i in range(len(sizes))]
+##axes[1].axis('tight')
+#axes[1].axis('off')
+#
+#times_formatted = np.transpose(np.array([np.array([0.0, 0.0, 0.0]) for i in range(len(sizes))]))
+#for row in range(times.shape[0]):
+#    for col in range(times.shape[1]):
+#        times_formatted[row][col] = "{0:5.3f}".format(times[row][col])
+#
+#axes[1].table(cellText=np.transpose(times_formatted), colLabels=col_labels, \
+#          rowLabels=row_labels, loc='center')
+#
+#fig.savefig('plot_timings.png')
